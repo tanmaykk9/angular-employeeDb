ffeebf483bc120709a4e34352a51e42949840b43
tanmaykk9 <tanmaykamath92@gmail.com>
2020-03-17 13:35:57 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { Services/employeeService } from './services/employee.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, EmployeeComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [Services/employeeService]
 })
 export class AppModule { }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Employee } from '../employee';
+import { EMPLOYEES } from '../employeeDB';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class employeeService {
+
+  constructor() { }
+
+}
